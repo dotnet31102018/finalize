a5b7285cecd4168dc2f55fe14837e1eda7aff0ec
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-02-28 20:31:32 +0200

    Add files via upload

---
+++ Adapter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] employeesArray = new string[5, 4]
+            {
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
+            };
+
+            Process(new SomeProcessor(), employeesArray);
+
+            Console.Read();
+        }
+        static void Process(ICommonProcessor processor, string[,] employeesArray)
+        {
+            processor.ProcessCompanySalary(employeesArray);
+        }
+    }
+
+}
