777220b0aa0120456eccc0a341d841d148f33f54
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-02-28 20:38:00 +0200

    Add files via upload

---
+++ RabbitProducer/Rabbit/Program.cs
@@ -0,0 +1,34 @@
+﻿using EasyNetQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rabbit
+{
+
+    class Program
+    {
+        static void SendMessage()
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                Console.WriteLine("Press any key to send the message");
+                Thread.Sleep(1500);
+                bus.Publish("{\"text\": \"Hello World\"}", "My_Game");
+                Console.WriteLine("Message sent ... Press any key to quit");
+            }
+        }
+      
+        static void Main(string[] args)
+        {
+
+            SendMessage();
+            SendMessage();
+
+        }
+
+    }
+}
